9130965d5306260267c305cea275b387edaeaf18
Tom Vincent <git@tlvince.com>
2015-06-09 11:01:51 +0100

    chore(cordova): add build scripts

---
+++ scripts/build-travis.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+info() { echo "$0: $1"; }
+error() { info "$1"; exit 1; }
+skip() { info "$1. Skipping build."; exit; }
+
+[[ "$TRAVIS" ]] || error "this script assumes its running within Travis"
+
+[[ "$TRAVIS_PULL_REQUEST" == "false" ]] || {
+  skip "This build was triggered by a pull request"
+}
+
+type="snapshot"
+[[ "$TRAVIS_TAG" ]] && type="release"
+[[ "$TRAVIS_BRANCH" == "staging" ]] && type="staging"
+
+[[ "$type" != "snapshot" ]] && {
+  openssl aes-256-cbc -K $encrypted_eecd1dce8c89_key -iv $encrypted_eecd1dce8c89_iv -in .android/release-files.tar.enc -out .android/release-files.tar -d
+  tar xvf .android/release-files.tar -C .android
+}
+
+./scripts/build.sh "$type"
+# Deploy only the resulting APK(s), not the interim artefacts
+rm -rf build/lomis
